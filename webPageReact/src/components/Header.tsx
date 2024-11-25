f9f361bb2778de0577db109157f1a778a02039b5
DavidPortatilCursoJava <davidmelgareslazaro@gmail.com>
2024-11-25 18:17:37 +0100

    HEader cambiado

--- webPageReact/src/components/Header.tsx
+++ webPageReact/src/components/Header.tsx
@@ -7,35 +7,31 @@ import Sidebar from "./Sidebar";
 
 import useToggle from "../hooks/useToggle";
 
-
 // Header with the logo on the left side, and a menu on the right. On mobile, the menu transforms into a hamburger icon that opens a sidebar.
 
-function Header () {
+function Header() {
   const [isOpenSidebar, toggleSidebar] = useToggle(false);
 
   return (
     <header className="bg-white text-black py-1">
       <div className="container px-2 sm:px-0 mx-auto flex items-center justify-between">
-        
-        
         <Logo />
-  
-        
+
         <div className="flex items-center gap-4 ml-auto">
           <div className="hidden sm:block">
             <Menu />
           </div>
 
-          
           {isOpenSidebar && <Sidebar toggle={toggleSidebar} />}
-          
-          
+
           <button onClick={toggleSidebar} className="sm:hidden">
             <FaBars size={30} />
           </button>
 
-          
-          <UserGreet name="Ivan" image="https://randomuser.me/api/portraits/men/3.jpg" />
+          <UserGreet
+            name="David"
+            image="https://randomuser.me/api/portraits/men/8.jpg"
+          />
         </div>
       </div>
     </header>
